c86ab7bf9fbd9005cad082e0dea084b82ab2a9ea
Denis Piskarev <timchamkin1@yandex.ru>
2024-02-02 14:39:53 +0300

    Delete App directory

--- App/database/migrations/0000001_create_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS "users" (
-    user_id INT PRIMARY KEY,
-    name VARCHAR(56) NOT NULL,
-    surname VARCHAR(56) NOT NULL,
-    patronymic VARCHAR(56) NOT NULL,
-    age INT NOT NULL,
-    gender VARCHAR(56) NOT NULL
-);
--- App/internal/API/api.go
+++
@@ -1,8 +0,0 @@
-package api
-
-import "github.com/gin-gonic/gin"
-
-func CreateUser(c *gin.Context) {
-	r := 1
-	r += 1
-}
